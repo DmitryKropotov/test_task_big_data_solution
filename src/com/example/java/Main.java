63d0c01d91cd7adeb76b3dbd726a7bf09e598a8b
Dmitry Kropotov <Dmitry_Kropotov@epam.com>
2018-09-19 23:52:56 +0300

    final version of realisation

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -11,48 +11,58 @@ import java.util.TreeMap;
 public class Main {
 
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new FileReader("src/com/example/java/TRD.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/com/example/java/TRD.csv"))) {
+
             TreeMap<LocalTime, Integer> sharesInTimeMoment = new TreeMap<>();
             HashMap<String, Integer> sharesPerExchange = new HashMap<>();
             HashMap<String, LocalTime> timeMomentOfFirstExchangeShare = new HashMap<>();
             HashMap<String, LocalTime> timeMomentOfLastExchangeShare = new HashMap<>();
+
             br.lines().map(string -> string.split(",")).forEach(strings -> {
-               LocalTime timeMoment = LocalTime.parse(strings[0]);
-               int amountOfShares = Integer.valueOf(strings[2]);
-               String exchange = strings[3];
-               sharesInTimeMoment.put(timeMoment, sharesInTimeMoment.getOrDefault(timeMoment, 0) + amountOfShares);
-               sharesPerExchange.put(exchange, sharesPerExchange.getOrDefault(exchange, 0) + amountOfShares);
-               timeMomentOfFirstExchangeShare.putIfAbsent(exchange, timeMoment);
-               timeMomentOfLastExchangeShare.put(exchange, timeMoment);
+                LocalTime timeMoment = LocalTime.parse(strings[0]);
+                int amountOfShares = Integer.valueOf(strings[2]);
+                String exchange = strings[3];
+                sharesInTimeMoment.put(timeMoment, sharesInTimeMoment.getOrDefault(timeMoment, 0) + amountOfShares);
+                sharesPerExchange.put(exchange, sharesPerExchange.getOrDefault(exchange, 0) + amountOfShares);
+                timeMomentOfFirstExchangeShare.putIfAbsent(exchange, timeMoment);
+                timeMomentOfLastExchangeShare.put(exchange, timeMoment);
             });
+
             LocalTime beginningOfSecond = sharesInTimeMoment.firstKey();
             int sharesInCurrentSecond = sharesInTimeMoment.get(beginningOfSecond);
             int maxShares = sharesInTimeMoment.get(beginningOfSecond);
-            LocalTime[] secondOfMaxShares = {beginningOfSecond,  beginningOfSecond.plusNanos(999000000L)};
-            LocalTime currentMoment = beginningOfSecond;
-            do {
-                currentMoment = sharesInTimeMoment.higherKey(currentMoment);
-                sharesInCurrentSecond =+ sharesInTimeMoment.get(currentMoment);
-                if (currentMoment.compareTo(beginningOfSecond.plusSeconds(1))>=0) {
-                    if(maxShares<sharesInCurrentSecond) {
+            LocalTime[] secondOfMaxShares = {beginningOfSecond, beginningOfSecond.plusNanos(999000000L)};
+            LocalTime currentMoment = sharesInTimeMoment.higherKey(beginningOfSecond);
+            while (currentMoment != null) {
+                if (currentMoment.compareTo(beginningOfSecond.plusSeconds(1)) >= 0) {
+                    if (maxShares < sharesInCurrentSecond) {
                         maxShares = sharesInCurrentSecond;
                         secondOfMaxShares[0] = beginningOfSecond;
                         secondOfMaxShares[1] = beginningOfSecond.plusNanos(999000000L);
                     }
-                    sharesInCurrentSecond =- sharesInTimeMoment.get(beginningOfSecond);
-                    beginningOfSecond = sharesInTimeMoment.higherKey(beginningOfSecond);
+                    do {
+                        sharesInCurrentSecond -= sharesInTimeMoment.get(beginningOfSecond);
+                        beginningOfSecond = sharesInTimeMoment.higherKey(beginningOfSecond);
+                    } while (currentMoment.compareTo(beginningOfSecond.plusSeconds(1)) >= 0);
                 }
-                sharesInCurrentSecond =+ sharesInTimeMoment.get(currentMoment);
-            } while (currentMoment.compareTo(sharesInTimeMoment.lastKey())!=0);
+                sharesInCurrentSecond += sharesInTimeMoment.get(currentMoment);
+                if (currentMoment == sharesInTimeMoment.lastKey() && maxShares < sharesInCurrentSecond) {
+                    maxShares = sharesInCurrentSecond;
+                    secondOfMaxShares[0] = beginningOfSecond;
+                    secondOfMaxShares[1] = beginningOfSecond.plusNanos(999000000L);
+                }
+                currentMoment = sharesInTimeMoment.higherKey(currentMoment);
+            }
 
             System.out.println(
                     "maximal amount of shares in 1 second was made between " + secondOfMaxShares[0] + " and " + secondOfMaxShares[1] +
-                            ". In this interval " + maxShares + " shares were done.");
+                            ". In this interval " + maxShares + " shares were made");
 
             Set<String> exchanges = sharesPerExchange.keySet();
             exchanges.forEach(exchange ->
-                   System.out.println("It was done "+sharesPerExchange.get(exchange)+" shares since "+timeMomentOfFirstExchangeShare.get(exchange)+
-            " till "+timeMomentOfLastExchangeShare.get(exchange)+" on exchange "+exchange));
+                    System.out.println(sharesPerExchange.get(exchange) + " shares were made since " +
+                            timeMomentOfFirstExchangeShare.get(exchange) +
+                            " till " + timeMomentOfLastExchangeShare.get(exchange) + " on exchange " + exchange));
         } catch (IOException e) {
             e.printStackTrace();
         }
